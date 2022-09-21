e888ab7375e7ae3612c2cca0bce27fe861be64f2
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2022-09-21 15:03:33 +0530

    system model updated

--- Model/System.js
+++ Model/System.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+const systemdata = new Schema({
+    sysNo: { type: String, required: true },
+    RAM: { type: Number, required: true },
+    processor: { type: String, required: true },
+    status: { type: String, required: true },
+    Lab: { type: Number, required: true },
+    serialNo: { type: Number, required: true },
+})
 const systemSchema = new Schema({
-    systemSpacs: { type: Number, required: true },
+    systemSpacs: { type: systemdata, required: true },
     bookedSlots: { type: [String], required: true },
 }, {
     timestamps: true,
 });
 
-const System = mongoose.model('System', SystemSchema);
+const System = mongoose.model('System', systemSchema);
 
 module.exports = System;
