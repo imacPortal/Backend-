def92c3e061eddab42988ee16129a60382d87080
Samaresh Samanta <ss9261@srmist.edu.in>
2022-11-09 14:51:13 +0530

    add: report functions

--- Routes/report.js
+++ Routes/report.js
@@ -10,4 +10,15 @@ router.route('/fetchAll').get((req,res)=>{
         .catch(err => res.status(400).json('Error: ' + err));
 })
 
+router.route('/fetchAll/:regNo').get((req,res)=>{
+
+    const regNo = req.params.regNo
+
+    Report.find({regNo})
+        .then((response)=>{
+            res.json({ status: "fetched", data:response, success: true })
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
 module.exports = router
